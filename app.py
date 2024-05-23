223be142189c21674cf72e6f24ef4b7bbf7593a2
Adnangad <gardobuyaadnan@gmail.com>
2024-05-23 15:03:32 +0300

    Message describing the changes

--- app.py
+++ app.py
@@ -129,4 +129,4 @@ def show_cart():
 if __name__ == '__main__':
     storage.reload()
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port)
