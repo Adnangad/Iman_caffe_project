13de9eec3e0e8a1040d4889d42554ac36b25a90c
Adnangad <gardobuyaadnan@gmail.com>
2024-05-30 20:50:07 +0300

    Recommitting changes after resolving corrupt object issue

--- app.py
+++ app.py
@@ -228,7 +228,13 @@ def is_valid_phone_number(phone_number):
 @app.route('/payment', methods=['POST'])
 def mobile_payment():
     phone_number = request.form.get('phone')
-    amount = request.form.get('amount')
+    user_id = session['user_id']
+    user = storage.get(User, user_id)
+    carts = sorted(list(storage.all(Cart).values()), key=lambda x: x.item)
+    amount = 0
+    for cart in carts:
+        if cart.user_id == user.id:
+            amount = amount + cart.price
     account_reference = 'TEST123'
     transaction_desc = 'Payment for supplies'
     if not is_valid_phone_number(phone_number):
